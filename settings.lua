f91fb7ebb8d0964c98297febafa3eea4271cf944
Pontus Laestadius <31597765+pontuslaestadius@users.noreply.github.com>
2020-08-16 18:31:46 +0200

    Add files via upload

---
+++ settings.lua
@@ -0,0 +1,22 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "chestsplotion-particles",
+        setting_type = "runtime-global",
+        default_value = true
+    }, {
+        type = "int-setting",
+        name = "chestsplotion-splash",
+        setting_type = "runtime-global",
+        default_value = 7,
+        minimum_value = 1,
+        maximum_value = 10
+    }, {
+        type = "int-setting",
+        name = "chestsplotion-frequency",
+        setting_type = "runtime-global",
+        default_value = 4,
+        minimum_value = 1,
+        maximum_value = 100
+    }
+})
